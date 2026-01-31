70f5deeb9b5b87aaca10ac381ceb4b3e483ead76
samithspalan <samithpoojary12345@gmail.com>
2026-01-31 09:48:11 +0530

    updated ui

---
+++ frontend/src/i18n/config.js
@@ -0,0 +1,20 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './locales/en.json'
+import kn from './locales/kn.json'
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      kn: { translation: kn }
+    },
+    lng: localStorage.getItem('language') || 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+export default i18n
